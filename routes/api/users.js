ee809bba38de0fcce2628dc7ee9176286d333a81
Isaac Hauser <isaac.hauser@live.com.au>
2019-12-16 22:38:36 +1000

    Working on routes

---
+++ routes/api/users.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const {
+    check,
+    validationResult
+} = require('express-validator/check');
+
+const User = require('../../models/User');
+
+// @route   POST api/users
+// @desc    Register User
+// @access  Public
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({
+        min: 6
+    })
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+    const {
+        name,
+        email,
+        password
+    } = req.body;
+
+    try {
+        let user = await User.findOne({
+            email
+        });
+
+        // See if user exists - if yes send error
+        if (user) {
+            return res.status(400).json({
+                errors: [{
+                    msg: 'User already exists '
+                }]
+            });
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        // Encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        // Save user to DB - await because it returns a promise
+        await user.save();
+
+        // Return jsonwebtoken
+
+        res.send('User Registered');
+    } catch (err) {
+        console.err(err.message);
+        res.status(500).send('Server Error');
+    }
+
+
+
+});
+
+module.exports = router;
